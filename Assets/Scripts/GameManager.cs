de3f68f468b62f5db683b267acb648c543742b8d
Dennis Hackethal <dh@dennishackethal.com>
2024-05-09 21:48:55 -0600

    Spawn obstacles automatically

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,11 +5,21 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public static bool ongoing = true;
+    public GameObject obstaclePrefab;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        int offset = 0;
+
+        for (int i = 0; i < 10; i++)
+        {
+            offset = (i + 1) * 5;
+
+            SpawnObstacle(offset);
+        }
+
+        StartCoroutine(SpawnObstacles(offset + 3));
     }
 
     // Update is called once per frame
@@ -22,5 +32,21 @@ public class GameManager : MonoBehaviour
     {
         ongoing = false;
         Debug.Log("Game over!");
+    }
+
+    private void SpawnObstacle(float x = 5f)
+    {
+        Vector3 spawnPosition = new Vector3(x, Random.Range(-2.8f, 4f), 0f);
+        Instantiate(obstaclePrefab, spawnPosition, Quaternion.identity);
+    }
+
+    private IEnumerator SpawnObstacles(float offset)
+    {
+        while (ongoing)
+        {
+            SpawnObstacle(offset);
+
+            yield return new WaitForSeconds(2f);
+        }
     }
 }
