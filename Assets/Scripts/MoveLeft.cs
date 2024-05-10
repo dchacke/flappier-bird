5a0df122db000869636f4a35f6f0e281fe9095b3
Dennis Hackethal <dh@dennishackethal.com>
2024-05-09 19:24:28 -0600

    Prevent moving left when game is over

--- Assets/Scripts/MoveLeft.cs
+++ Assets/Scripts/MoveLeft.cs
@@ -15,6 +15,9 @@ public class MoveLeft : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.left * speed * Time.deltaTime);
+        if (GameManager.ongoing)
+        {
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+        }
     }
 }
