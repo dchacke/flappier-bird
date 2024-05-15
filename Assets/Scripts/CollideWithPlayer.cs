419dd771221754442ac7a665ed865a9ab0231968
Dennis Hackethal <dh@dennishackethal.com>
2024-05-14 19:05:27 -0600

    Housekeeping

--- Assets/Scripts/CollideWithPlayer.cs
+++ Assets/Scripts/CollideWithPlayer.cs
@@ -18,7 +18,7 @@ public class CollideWithPlayer : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
+        if (collision.gameObject.CompareTag("Player") && GameManager.ongoing)
         {
             GameManager.GameOver();
         }
